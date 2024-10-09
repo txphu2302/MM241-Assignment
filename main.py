27d8adad50eb8995e067081c9de1371f31cad445
Martin Nguyen <nguyenquangduc2000@gmail.com>
2024-10-09 16:08:00 -0700

    Update code

--- main.py
+++ main.py
@@ -8,35 +8,38 @@ env = gym.make(
     "gym_cutting_stock/CuttingStock-v0",
     render_mode="human",  # Comment this line to disable rendering
 )
+NUM_EPISODES = 100
 
 if __name__ == "__main__":
     # Reset the environment
     observation, info = env.reset(seed=42)
-    print(info)
 
     # Test GreedyPolicy
     gd_policy = GreedyPolicy()
-    for _ in range(200):
+    ep = 0
+    while ep < NUM_EPISODES:
         action = gd_policy.get_action(observation, info)
         observation, reward, terminated, truncated, info = env.step(action)
-        print(info)
 
         if terminated or truncated:
-            observation, info = env.reset()
+            observation, info = env.reset(seed=ep)
+            print(info)
+            ep += 1
 
     # Reset the environment
     observation, info = env.reset(seed=42)
-    print(info)
 
     # Test RandomPolicy
     rd_policy = RandomPolicy()
-    for _ in range(200):
+    ep = 0
+    while ep < NUM_EPISODES:
         action = rd_policy.get_action(observation, info)
         observation, reward, terminated, truncated, info = env.step(action)
-        print(info)
 
         if terminated or truncated:
-            observation, info = env.reset()
+            observation, info = env.reset(seed=ep)
+            print(info)
+            ep += 1
 
     # Uncomment the following code to test your policy
     # # Reset the environment
