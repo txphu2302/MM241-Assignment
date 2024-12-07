98edec0119f64cffe2bb6231fc2d26563c686fbe
txphu2302 <phu.tran232@hcmut.edu.vn>
2024-12-07 13:14:41 +0700

    TODO policy2210xxx.py

--- main.py
+++ main.py
@@ -12,47 +12,48 @@ NUM_EPISODES = 100
 
 if __name__ == "__main__":
     # Reset the environment
-    observation, info = env.reset(seed=42)
-
-    # Test GreedyPolicy
-    gd_policy = GreedyPolicy()
-    ep = 0
-    while ep < NUM_EPISODES:
-        action = gd_policy.get_action(observation, info)
-        observation, reward, terminated, truncated, info = env.step(action)
-
-        if terminated or truncated:
-            print(info)
-            observation, info = env.reset(seed=ep)
-            ep += 1
-
-    # Reset the environment
-    observation, info = env.reset(seed=42)
-
-    # Test RandomPolicy
-    rd_policy = RandomPolicy()
-    ep = 0
-    while ep < NUM_EPISODES:
-        action = rd_policy.get_action(observation, info)
-        observation, reward, terminated, truncated, info = env.step(action)
-
-        if terminated or truncated:
-            print(info)
-            observation, info = env.reset(seed=ep)
-            ep += 1
-
-    # Uncomment the following code to test your policy
-    # # Reset the environment
     # observation, info = env.reset(seed=42)
-    # print(info)
 
-    # policy2210xxx = Policy2210xxx()
-    # for _ in range(200):
-    #     action = policy2210xxx.get_action(observation, info)
+    # # Test GreedyPolicy
+    # gd_policy = GreedyPolicy()
+    # ep = 0
+    # while ep < NUM_EPISODES:
+    #     action = gd_policy.get_action(observation, info)
     #     observation, reward, terminated, truncated, info = env.step(action)
-    #     print(info)
 
     #     if terminated or truncated:
-    #         observation, info = env.reset()
+    #         print(info)
+    #         observation, info = env.reset(seed=ep)
+    #         ep += 1
+
+    # # Reset the environment
+    # observation, info = env.reset(seed=42)
+
+    # # Test RandomPolicy
+    # rd_policy = RandomPolicy()
+    # ep = 0
+    # while ep < NUM_EPISODES:
+    #     action = rd_policy.get_action(observation, info)
+    #     observation, reward, terminated, truncated, info = env.step(action)
+
+    #     if terminated or truncated:
+    #         print(info)
+    #         observation, info = env.reset(seed=ep)
+    #         ep += 1
+
+    # Uncomment the following code to test your policy
+    # Reset the environment
+    observation, info = env.reset(seed=42)
+    print(info)
+
+    policy2210xxx = Policy2210xxx()
+    for _ in range(200):
+        action = policy2210xxx.get_action(observation, info)
+        observation, reward, terminated, truncated, info = env.step(action)
+        print(info)
+
+        if terminated or truncated:
+            observation, info = env.reset()
+            break
 
 env.close()
--- student_submissions/s2210xxx/policy2210xxx.py
+++ student_submissions/s2210xxx/policy2210xxx.py
@@ -1,14 +1,46 @@
 from policy import Policy
 
-
-def Policy2210xxx(Policy):
+class Policy2210xxx(Policy):
     def __init__(self):
         # Student code here
-        pass
+        self.sorted_prods = []
+        self.sorted = False
 
     def get_action(self, observation, info):
-        # Student code here
-        pass
+        list_prods = observation["products"]
+        stock_idx = -1
 
-    # Student code here
-    # You can add more functions if needed
+        if not self.sorted:
+            for prod in list_prods:
+                prod_size = prod["size"]
+                prod_w, prod_h = prod_size
+                prod_area = prod_w * prod_h
+                self.sorted_prods.append((prod_area, prod))
+
+            # Sort products by area in descending order
+            self.sorted_prods.sort(key=lambda x: x[0], reverse=True)
+            self.sorted = True  # Đánh dấu là đã sắp xếp
+
+        for prod in self.sorted_prods:
+            prod_width, prod_height = prod[1]["size"]
+            placed = False
+            x, y = -1, -1  # Initialize x and y
+            if prod[1]["quantity"] > 0:
+                for i, stock in enumerate(observation["stocks"]):
+                    stock_w, stock_h = self._get_stock_size_(stock)
+                    for (w, h) in [(prod_width, prod_height), (prod_height, prod_width)]:
+                        for x in range(stock_w - w + 1):
+                            for y in range(stock_h - h + 1):
+                                if self._can_place_(stock, (x, y), (w, h)):
+                                    stock_idx = i
+                                    placed = True
+                                    print("Product size: ", (w, h), "Quantity: ", prod[1]["quantity"])
+                                    return {"stock_idx": stock_idx, "size": (w, h), "position": (x, y)}
+                            if placed:
+                                break
+                        if placed:
+                            break
+                    if placed:
+                        break
+
+        return {"stock_idx": stock_idx, "size": prod[1]["size"], "position": (x, y)}
